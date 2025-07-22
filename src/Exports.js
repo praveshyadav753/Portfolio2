e5d6e14a00d1194abad21f24c598955d2d766844
pravesh yadav <129743521+praveshyadav753@users.noreply.github.com>
2025-07-22 16:35:39 +0530

    Update Exports.js

--- src/Exports.js
+++ src/Exports.js
@@ -2,7 +2,9 @@ import Intro from "./components/home/intro";
 import Navbar from "./components/navbar/navbar"
 import AboutMe from "./components/aboutme/aboutme";
 import SkillsSection from "./components/skills/skills";
-import WorkExperience from "./components/work/work";
+import WorkExperience from "./components/work
+/work.jsx"
+
 import ContactMe from "./components/contact/contactme";
 import Gitactivity from "./components/github/activity";
 import ProjectsSection from "./components/projects/projects";
